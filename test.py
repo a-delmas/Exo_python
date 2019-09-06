83bd20761a782653b57ce28b85b8753942a0eb4f
a-delmas <aurore.delmas@hotmail.fr>
2019-09-06 13:32:48 +0200

    Exo

---
+++ test.py
@@ -0,0 +1,29 @@
+# n = 100
+# stars = ""
+# for i in range(n):
+#     for nb_spaces in range(n-i-1):
+#         stars = stars + " "
+#     for nb_stars in range(2*i-1):
+#         stars = stars + "*"
+#     stars = stars +"\n"
+# print(stars)
+
+# temp = 18
+# if temp < 20 :
+#     print("J'ai froid")
+# if temp > 30 :
+#     print("J'ai chaud")
+# else :
+#     print("Je suis bien")
+
+# for i in range(100):
+#     if i % 2 == 0: 
+#         print(i,"pair")
+#     else:
+#         print(i, "impair") 
+
+for i range(10) :
+    print("Table de "+ str 'i))
+    for j range(10) :
+        sum i*j:
+        print(str(i) + "*" + str(j), "=", sum i*j)
