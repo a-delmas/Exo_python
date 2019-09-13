7e0b97e36fed6b8e1a2c28c101f22331292867b3
a-delmas <aurore.delmas@hotmail.fr>
2019-09-13 15:19:21 +0200

    Exercice

--- exercice.py
+++ exercice.py
@@ -1,5 +1,5 @@
-maliste = [0, 11, 12, 30, 34, 48, 66]
-# mynumber = 9
+# maliste = [0, 11, 12, 30, 34, 48, 66]
+# # mynumber = 9
 # cpt = 0
 # for i in range(len(maliste)):
 #     if maliste[i] > mynumber:
@@ -20,18 +20,69 @@ maliste = [0, 11, 12, 30, 34, 48, 66]
 
 # print(is_ordered)
 
-mynumber = 9
+# mynumber = 9
+# # for i in range(len(maliste)):
+# #     if mynumber == maliste[i]:
+# #         print([i])
+# #     else:
+# #         print(False)
+
+# is_found = False
+
 # for i in range(len(maliste)):
-#     if mynumber == maliste[i]:
-#         print([i])
-#     else:
-#         print(False)
+#     if maliste[i] > mynumber:
+#         is_found = False
 
-is_found = False
+# print(is_found)
+#     print(maliste.index(mynumber))
 
-for i in range(len(maliste)):
-    if maliste[i] > mynumber:
-        is_found = False
+            # resultat = 0
+            # for i in range(len(maliste)):
+            #     if maliste[i]%n == 0:
+            #         resultat += i
+            # print(str(i) + "est divisible par" + "n")
+            #     else:
+            # print(str(i) + "n'est pas divisible par" + "n")
 
-print(is_found)
-    print(maliste.index(mynumber))
+# l = []
+# nb_elements = 9
+# import random
+# for i in range(nb_elements):
+#     l.append(random.randint (1, 100))
+# print(l)
+
+l1 = [2, 4, 9, 10, 5, 12]
+l2 = []
+
+                                # for i in range(len(l1)):
+                                #     if l1[i] % 2 == 0: 
+                                #         print(i,"pair")
+                                #     else:
+                                #         print(i,"impair") 
+
+                                # for i in range(len(l2)):
+                                #     l2.append(l1[i] == "pair")
+                                #     print(l2)
+                                    
+                                #     l1.pop(l2 = l1[-1])
+                                #     print(l2)
+
+                                #                 # if l1[i] == "pair":
+                                #                 #     print(i)
+                                #                 # else:
+                                #                 #     print(a.pop[i])
+
+for i in l1:
+    if i % 2 != 0:
+        l2.append(i)
+        l1.remove(i)
+
+for i in range(len(l1)):
+    if l1[i] % 2 = 0:
+        l2.append(l1[i])
+
+for i in range(len([l2])):
+    l1.remove(l2[i])
+
+print(l1)
+print(set(l2))
